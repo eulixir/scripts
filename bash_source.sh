31c92c8cca59119e52c38b90ea6af79ea3374aa4
Joao Pedro Alves <jotalmeida007@hotmail.com>
2021-12-16 13:21:08 -0300

    chore: update envs into bash and zshrc

---
+++ bash_source.sh
@@ -0,0 +1,3 @@
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+source ~/.bashrc
---
+++ zsh_source.sh
@@ -0,0 +1,3 @@
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+source ~/.zshrc
