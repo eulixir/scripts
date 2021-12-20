fbff687e211b773d284168e740e7ee55e6b9e475
João Pedro Alves <jotalmeida007@hotmail.com>
2021-12-20 13:25:25 -0300

    Delete asdf.sh

--- Install asdf/asdf.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update
-
-sudo apt-get -y install build-essential inotify-tools autoconf m4 libncurses5-dev libwxgtk-webview3.0-gtk3-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
-
-sudo apt install git
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf
-git checkout "$(git describe --abbrev=0 --tags)"
