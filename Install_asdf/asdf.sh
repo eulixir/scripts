2560623d13ad00b2d3aaf1c6e9ca6e6463f5d87c
João Pedro Alves <jotalmeida007@hotmail.com>
2021-12-24 13:28:01 -0300

    chore: move bash source to asdf folder

--- Install_asdf/asdf.sh
+++ Install_asdf/asdf.sh
@@ -4,3 +4,7 @@ sudo apt install git
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 cd ~/.asdf
 git checkout "$(git describe --abbrev=0 --tags)"
+
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+source ~/.bashrc
--- bash_source.sh
+++
@@ -1,3 +0,0 @@
-echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-source ~/.bashrc
