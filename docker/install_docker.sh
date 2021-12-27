44c5717fbdf73bcc5ec11ce1d270fed4c48c6d29
João Pedro Alves <jotalmeida007@hotmail.com>
2021-12-27 13:31:01 -0300

    move docker script to docker folder

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -5,8 +5,6 @@ sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
-sudo docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
-
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
