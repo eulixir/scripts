aedde34f06165d5fe734475d84dc5b10b06ab963
João Pedro Alves <jotalmeida007@hotmail.com>
2021-12-31 20:43:42 -0300

    chore: update erlang version

--- techs/erlang/install.sh
+++ techs/erlang/install.sh
@@ -1,5 +1,5 @@
 sudo apt-get -y install build-essential inotify-tools autoconf m4 libncurses5-dev libwxgtk-webview3.0-gtk3-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
 
 asdf plugin add erlang 
-asdf install erlang 24.0
-asdf global erlang 24.0
+asdf install erlang 24.2
+asdf global erlang 24.2
