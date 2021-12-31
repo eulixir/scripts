7726aa3b65b1c612cdfad5dfc476a82f203ad5b2
João Pedro Alves <jotalmeida007@hotmail.com>
2021-12-30 21:58:28 -0300

    chore: add zsh installer

---
+++ oh_my_zsh/install.sh
@@ -0,0 +1,10 @@
+sudo apt install curl
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+
