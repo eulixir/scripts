8d065df50185b4b44d3b790313086091923b2606
João Pedro Alves <jotalmeida007@hotmail.com>
2021-12-30 21:59:28 -0300

    chore: remove extra line on docker script

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # scripts
 
 Script to make your day more productive
+
+## to do
+
+- Make a Cli
--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -6,5 +6,3 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
