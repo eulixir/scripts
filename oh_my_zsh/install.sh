85600f216bdc8f9ad37aade4fa3856149f0d5c65
João Pedro Alves <jotalmeida007@hotmail.com>
2022-01-01 23:30:10 -0300

    chore: remove zinit

--- oh_my_zsh/install.sh
+++ oh_my_zsh/install.sh
@@ -10,8 +10,5 @@ git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/theme
 
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)" -y
-
-
 
 
